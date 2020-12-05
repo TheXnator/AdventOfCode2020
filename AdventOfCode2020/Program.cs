5612a9e9e399358c6b977b3a94d0b2385ef34343
TheXnator <trexnatorgaming@gmail.com>
2020-12-05 01:07:59 +0000

    day 4 p 2

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -23,6 +24,7 @@ namespace AdventOfCode2020
 
             // Day 4
             Console.WriteLine(String.Format("Day 4 p. 1: {0}", GetValidPassports()));
+            Console.WriteLine(String.Format("Day 4 p. 2: {0}", GetValidPassportsNew()));
         }
 
         static int Get2020PairProduct()
@@ -147,6 +149,74 @@ namespace AdventOfCode2020
                     if (!dat.Contains(field))
                     {
                         isValid = false;
+                    }
+                }
+
+                if (isValid)
+                {
+                    valid++;
+                }
+            }
+
+            return valid;
+        }
+
+        static int GetValidPassportsNew()
+        {
+            string filename = "day4inputs.txt";
+            int valid = 0;
+
+            string contents = File.ReadAllText(filename);
+            string[] data = contents.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            string[] eyeCols = new string[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+            Dictionary<string, int[]> requiredFields = new Dictionary<string, int[]>();
+            requiredFields.Add("byr", new int[3] { 1920, 2002, 4 });
+            requiredFields.Add("iyr", new int[3] { 2010, 2020, 4 });
+            requiredFields.Add("eyr", new int[3] { 2020, 2030, 4 });
+            requiredFields.Add("hgt", new int[3] { 0, 0, 0 });
+            requiredFields.Add("hcl", new int[3] { 0, 0, 0 });
+            requiredFields.Add("ecl", new int[3] { 0, 0, 0 });
+            requiredFields.Add("pid", new int[3] { 0, 0, 9 });
+
+            foreach (string dat in data)
+            {
+                bool isValid = true;
+
+                foreach (KeyValuePair<string, int[]> field in requiredFields)
+                {
+                    if (!dat.Contains(field.Key))
+                    {
+                        isValid = false;
+                    }
+                    else
+                    {
+                        string fieldData = dat.Split(field.Key + ":")[1].Split(new string[]{ " ", "\n" }, StringSplitOptions.RemoveEmptyEntries)[0];
+                        fieldData = Regex.Escape(fieldData).Replace("\\r", "");
+                        fieldData = Regex.Escape(fieldData).Replace("\\", "");
+
+                        for (int pos = 0; pos < 3; pos++)
+                        {
+                            int i = field.Value[pos];
+                            if (i > 0 && isValid)
+                            {
+                                isValid = (pos == 0) ? (i <= Convert.ToInt32(fieldData)) : ((pos == 1) ? (i >= Convert.ToInt32(fieldData)) : fieldData.Length == i);
+                            }
+                        }
+
+                        if (field.Key == "hgt" && (isValid))
+                        {
+                            bool cm = fieldData.Contains("cm");
+                            isValid = (fieldData.Contains("cm") || fieldData.Contains("in")) && (Convert.ToInt32(fieldData.Split(cm ? "cm" : "in")[0]) >= (cm ? 150 : 59) && Convert.ToInt32(fieldData.Split(cm ? "cm" : "in")[0]) <= (cm ? 193 : 76));
+                        }
+                        else if (field.Key == "hcl" && (isValid))
+                        {
+                            isValid = fieldData[0] == '#' && Regex.Split(fieldData.Split('#')[1], @"[a-f\d]").Length == 7;
+                        }
+                        else if (field.Key == "ecl" && (isValid))
+                        {
+                            isValid = (Array.IndexOf(eyeCols, fieldData) > -1);
+                        }
                     }
                 }
 
